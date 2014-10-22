f8a331e448473341a992220335462530a40ec496
John Lusk <john-github@how-hard-can-it-be.com>
2014-10-22 07:18:15 -0400

    Figured out classpath issue. First pass at command-line argument handling.

--- src/main/java/com/how_hard_can_it_be/primes/App.java
+++ src/main/java/com/how_hard_can_it_be/primes/App.java
@@ -9,11 +9,20 @@ import org.kohsuke.args4j.CmdLineParser;
  */
 public class App 
 {
-    public static void main( String[] args ) throws CmdLineException
+    public static void main( String[] args )
     {
     	Options options = new Options();
     	CmdLineParser parser = new CmdLineParser( options);
-    	parser.parseArgument(args);
+    	try 
+    	{
+			parser.parseArgument(args);
+		}
+    	catch (CmdLineException exc) 
+    	{
+    		System.err.println( exc.getMessage());
+    		System.err.println("primes [options]");
+    		parser.printUsage(System.err);
+		}
         System.out.println( "Hello World!" );
     }
 }
